fa090fa2f5a338fb4769b129cb1df5d2ec32a71f
Christopher Straub <ccstraub@gmail.com>
2021-08-23 17:11:04 -0500

    Implemented handleChangeCpassword controller

--- controllers/profile.js
+++ controllers/profile.js
@@ -11,7 +11,28 @@ const handleChangeDisplayName = (database) => (req, res) => {
 };
 
 const handleChangePassword = (database, bcrypt) => (req, res) => {
-  res.send('Change password');
+  const { id, password, new_password } = req.body;
+
+  if (password === new_password) res.sendStatus(400);
+  else {
+    database('app_user')
+      .where('id', id)
+      .select('hash')
+      .then((hashes) =>
+        hashes.length ? hashes[0].hash : Promise.reject(Error())
+      )
+      .then((hash) => bcrypt.compare(password, hash))
+      .then((result) =>
+        result ? bcrypt.hash(new_password, 10) : Promise.reject(Error())
+      )
+      .then((newHash) =>
+        database('app_user')
+          .where('id', id)
+          .update({ hash: newHash })
+          .then(() => res.sendStatus(200))
+      )
+      .catch((error) => res.sendStatus(400));
+  }
 };
 
 export { handleChangeDisplayName, handleChangePassword };

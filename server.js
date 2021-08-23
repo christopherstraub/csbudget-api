aa5d621f659a3431bba84ff35e469eea69f9d4ec
Christopher Straub <ccstraub@gmail.com>
2021-08-23 18:57:30 -0500

    Changed naming convention

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import express from 'express';
 import bcrypt from 'bcryptjs';
 import cors from 'cors';
 
-import handleSignUp from './controllers/signup.js';
-import handleSignIn from './controllers/signin.js';
+import handleSignUp from './controllers/sign-up.js';
+import handleSignIn from './controllers/sign-in.js';
 import {
   handleCreateBudget,
   handleDeleteBudget,
@@ -20,13 +20,13 @@ import database from './database.js';
 const app = express();
 app.use(express.json());
 
-app.post('/signup', handleSignUp(database, bcrypt));
-app.post('/signin', handleSignIn(database, bcrypt));
+app.post('/sign-up', handleSignUp(database, bcrypt));
+app.post('/sign-in', handleSignIn(database, bcrypt));
 app.post('/budget', handleCreateBudget(database));
 app.delete('/budget', handleDeleteBudget(database));
 app.put('/budget', handleSaveBudget(database));
 app.put('/budgets', handleSaveBudgets(database));
-app.put('/displayname', handleChangeDisplayName(database));
+app.put('/display-name', handleChangeDisplayName(database));
 app.put('/password', handleChangePassword(database, bcrypt));
 
 app.listen(3001);

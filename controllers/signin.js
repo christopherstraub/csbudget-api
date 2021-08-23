b6d1b0260bb5c48bc5c2b260958bebe9d9d1310d
Christopher Straub <ccstraub@gmail.com>
2021-08-23 18:28:36 -0500

    Implemented handleSignIn controller

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,5 +1,31 @@
 const handleSignIn = (database, bcrypt) => (req, res) => {
-  res.send('Sign in');
+  const { username, password } = req.body;
+
+  // Get the user's current hash.
+  database('app_user')
+    .where('username', username)
+    .select('hash')
+    .then((hashes) =>
+      hashes.length ? hashes[0].hash : Promise.reject(Error())
+    )
+    // Compare incoming password to hash.
+    .then((hash) => bcrypt.compare(password, hash))
+    // If valid, return user.
+    .then((result) =>
+      result
+        ? database('app_user')
+            .where('username', username)
+            .select(
+              'id',
+              'username',
+              'display_name',
+              'joined',
+              'current_budget_index'
+            )
+        : Promise.reject(Error())
+    )
+    .then((users) => res.json(users[0]))
+    .catch((error) => res.sendStatus(400));
 };
 
 export default handleSignIn;

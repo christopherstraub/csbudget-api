872141d802b1761bfc7585c1a112f71ca8b78425
Christopher Straub <ccstraub@gmail.com>
2021-08-23 18:50:30 -0500

    Implemented handleSignUp controller

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,6 +1,21 @@
 const handleSignUp = (database, bcrypt) => (req, res) => {
-  database.select().from('app_user').then(console.log).catch(console.log);
-  res.json('Sign up');
+  const { username, password, display_name } = req.body;
+
+  bcrypt
+    .hash(password, 10)
+    .then((hash) =>
+      database('app_user').insert(
+        {
+          username,
+          hash,
+          display_name,
+          join_date: new Date(),
+        },
+        ['id', 'username', 'display_name', 'join_date', 'current_budget_index']
+      )
+    )
+    .then((users) => res.json(users[0]))
+    .catch((error) => res.sendStatus(400));
 };
 
 export default handleSignUp;

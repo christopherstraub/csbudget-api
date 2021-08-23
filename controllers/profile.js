ee97598d2b3e0d622b022e335db107bae6bf799a
Christopher Straub <ccstraub@gmail.com>
2021-08-23 15:43:19 -0500

    Implemented handleChangeDisplayName controller

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,5 +1,13 @@
 const handleChangeDisplayName = (database) => (req, res) => {
-  res.send('Change display name');
+  const { id, display_name } = req.body;
+
+  database('app_user')
+    .where('id', id)
+    .update({ display_name }, ['display_name'])
+    .then((displayNames) =>
+      displayNames.length ? res.json(displayNames[0]) : res.sendStatus(404)
+    )
+    .catch((error) => res.sendStatus(400));
 };
 
 const handleChangePassword = (database, bcrypt) => (req, res) => {
